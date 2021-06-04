1a16d116189c6a60e2c84815da68f680c325b96f
dsterentev <danilterentyev25@gmail.com>
2021-06-04 18:35:01 +0900

    second

---
+++ src/primeru/Nomer3.java
@@ -0,0 +1,28 @@
+package primeru;
+import java.util.Scanner;
+public class Nomer3 {
+    public static Scanner sc;
+    public static int[] arr;
+
+    public static void fillArr(int i){
+        if(i < arr.length){
+            System.out.println("Введите " + (i+1) + " элемент");
+            arr[i] = sc.nextInt();
+            fillArr(++i);
+        }
+    }
+
+    public static void printArr(int i){
+        if(i < arr.length){
+            System.out.println(arr[i]);
+            printArr(++i);
+        }
+    }
+
+    public static void main(String[] args){
+        sc = new Scanner(System.in);
+        arr = new int[5];
+        fillArr(0);
+        printArr(0);
+    }
+}
---
+++ src/primeru/perevod.java
@@ -0,0 +1,21 @@
+package primeru;
+import java.util.Scanner;
+public class perevod {
+    public static String str = "";
+    public static Scanner sc;
+
+    public static String decToBin(int n){
+        if(n/2!=0){
+            decToBin(n/2);
+        }
+        str = str.concat(Integer.toString(n%2));
+        return str;
+    }
+
+    public static void main(String[] args){
+        sc = new Scanner(System.in);
+        System.out.println("Введите число для перевода");
+        int n = sc.nextInt();
+        System.out.println(decToBin(n));
+    }
+}
